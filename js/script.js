43ab428858d4a181effeb32ef774224c31bf958d
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2020-11-17 17:06:39 +0100

    Add 'find the correct article using the selector' script

--- js/script.js
+++ js/script.js
@@ -32,7 +32,9 @@ const titleClickHandler = function(event){
     console.log(articleSelector);
   
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
     /* add class 'active' to the correct article */
 }
   
