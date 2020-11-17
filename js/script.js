feee37b692634983faf37cf566f59a64b7bacb53
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2020-11-17 17:25:33 +0100

    Add 'add class active to the correct article' script

--- js/script.js
+++ js/script.js
@@ -31,15 +31,15 @@ const titleClickHandler = function(event){
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
   
-const links = document.querySelectorAll('.titles a');
-  
+const links = document.querySelectorAll('.titles a');  
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
