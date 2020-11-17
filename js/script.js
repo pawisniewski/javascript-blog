dc9ca07b9e9a2f941e7363c3e71734e141eadaf8
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2020-11-17 16:44:19 +0100

    Add 'get href attribute from the clicked link' script

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.getElementById('test-button').addEventListener('click', function(){
   });
 */
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -26,7 +27,9 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
   
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
