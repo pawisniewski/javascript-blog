be6191ac7c2e55969bb8875c88ab0dc54bf414b7
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2020-11-25 13:14:32 +0100

    Fix 'generateTags' function

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
 
 
   const generateTitleLinks = function(customSelector = ''){
@@ -78,6 +78,9 @@
 
   const generateTags = function(){
 
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
+
     /* [DONE] find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
 
@@ -105,6 +108,15 @@
         /* [DONE] add generated code to html variable */
         html = html + linkHTML;
 
+        /* [NEW] check if this link is NOT already in allTags */
+        if(!allTags[tag]) {
+
+          /* [NEW] add tag to allTags object */
+          allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
+        }
+
       /* [DONE] END LOOP: for each tag */
       }
 
@@ -113,7 +125,26 @@
 
     /* [DONE] END LOOP: for every article: */
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + '(' + allTags[tag] + ')</a></li>';
+
+    /* [NEW] END LOOP: for each tag in allTags: */
+    }
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+
   };
+
   generateTags();
 
 
@@ -271,4 +302,5 @@
     }
   };
   addClickListenersToAuthors();
+
 }
